e2832de8541424aff3e0007086b97241a0028299
Rustam <rust66637@gmail.com>
2020-05-09 02:12:03 +0300

    upd

--- blog/views.py
+++ blog/views.py
@@ -3,16 +3,39 @@ from django.urls import reverse
 from django.views import View
 from .models import Post
 from .forms import PostForm
+from django.core.paginator import Paginator
 
 
 class PostList(View):
 
     def get(self, request):
-        posts = Post.objects.all()
+
         firstpost = Post.objects.first()
+        posts = Post.objects.all()[1:]
+
+        paginator = Paginator(posts, 1)
+
+        page_number = request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+        # есть ли другие страницы
+        is_paginated = page.has_other_pages()
+        # есть ли пред. стр
+        if page.has_previous():
+            prev_url = f'?page={page.previous_page_number()}'
+        else:
+            prev_url = ''
+        # есть ли след. стр
+        if page.has_next():
+            next_url = f'?page={page.next_page_number()}'
+        else:
+            next_url = ''
+
         context = {
             'posts': posts,
             'firstpost': firstpost,
+            'is_paginated': is_paginated,
+            'next_url': next_url,
+            'prev_url': prev_url,
         }
         return render(request, 'blog/index.html', context=context)
 
